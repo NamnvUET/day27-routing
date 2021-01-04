4e903e3cb1bfae16968c68127582861a9126c7f3
namnv <nguyenvannamk59@gmail.com>
2021-01-04 14:57:39 +0700

    add bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
+import { ArticleListComponent } from './article-list/article-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "detail",
+    component: ArticleDetailComponent,
+  },
+  {
+    path: "",
+    component: ArticleListComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
