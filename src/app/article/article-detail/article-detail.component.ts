9d20ce4cb8b31380c80b6aee3d02578cee52f599
namnv <nguyenvannamk59@gmail.com>
2021-01-07 15:43:27 +0700

    compare 2 cach retrieve param trong activatedRoute

--- src/app/article/article-detail/article-detail.component.ts
+++ src/app/article/article-detail/article-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ArticleService } from '../article.service';
 import { Article } from '../../model/article';
+import { map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-article-detail',
@@ -14,9 +15,20 @@ export class ArticleDetailComponent implements OnInit {
 
   constructor(private _route: ActivatedRoute, private _api: ArticleService) { }
 
+  //Cách dùng snapshot để retrieve params trên ActivatedRoute
+  // ngOnInit(): void {
+  //   let slug = this._route.snapshot.paramMap.get("slug");
+  //   this.article$ = this._api.getArticleBySlug(slug);
+  // }
+
+  //Cách dùng observable để retrieve params
   ngOnInit(): void {
-    let slug = this._route.snapshot.paramMap.get("slug");
-    this.article$ = this._api.getArticleBySlug(slug);
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.article$ = this._route.paramMap.pipe(
+      map(params => params.get('slug')),
+      switchMap(slug => this._api.getArticleBySlug(slug))
+    );
   }
 
 }
